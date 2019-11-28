bd01316026d04fb52c39193083dea2131ac541bb
MKaramen <matis.kara@gmail.com>
2019-11-28 12:34:34 +0100

    end crud + error handling

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,10 +4,39 @@ const mongoose = require('mongoose')
 
 const Product = require('../models/product');
 
-router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "We got the GET to products"
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    const all_products = await Product.find().select('_id name price')
+
+    const response = {
+      count: all_products.length,
+      product: all_products.map(product => {
+        return {
+          _id: product.id,
+          name: product.name,
+          price: product.price,
+          request: {
+            type: "GET",
+            url: 'http://localhost:3000/products/' + product.id
+          }
+
+        }
+      })
+    };
+
+    if (all_products.length > 0) {
+      res.status(200).json(response)
+    } else {
+      res.status(404).json({
+        error: "DataBase is empty"
+      })
+    };
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    })
+  }
 });
 
 router.post("/", async (req, res, next) => {
@@ -22,10 +51,19 @@ router.post("/", async (req, res, next) => {
   try {
     await product.save();
     res.status(201).json({
-      message: "We got the POST to products",
-      createdProduct: product
+      message: "Created object",
+      createdProduct: {
+        name: product.name,
+        price: product.price,
+        id: product._id,
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/products/' + product.id
+        }
+      }
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json({
       error: err
     })
@@ -38,7 +76,13 @@ router.get("/:productId", async (req, res, next) => {
   try {
     const id = req.params.productId;
     const find_product = await Product.findById(id);
-    res.status(200).json(find_product);
+    if (find_product) {
+      res.status(200).json(find_product);
+    } else {
+      res.status(404).json({
+        message: "The id doesn't exist"
+      })
+    }
 
   } catch (err) {
     console.error(err);
@@ -46,16 +90,52 @@ router.get("/:productId", async (req, res, next) => {
 
 });
 
-router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated product"
-  });
+router.patch("/:productId", async (req, res, next) => {
+  try {
+    const id = req.params.productId;
+
+    const propertiesToUpdate = {};
+
+    // console.log(req.body) => [ { propName: 'price', value: 50 } ]
+
+
+    for (const properties of req.body) {
+      // console.log(properties) => { propName: 'price', value: 50 } 
+      propertiesToUpdate[properties.propName] = properties.value;
+    }
+
+    const updated_product = await Product.updateOne({
+      _id: id
+    }, propertiesToUpdate);
+    res.status(200).json({
+      message: "Updated product",
+      product: updated_product
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Deleted product"
-  });
+router.delete("/:productId", async (req, res, next) => {
+  try {
+    // Get ID from URL 
+    const id = req.params.productId;
+
+    const deleted_item = await Product.deleteOne({
+      _id: id
+    });
+
+    res.status(200).json({
+      message: "Deleted product",
+      item: deleted_item
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.json({
+      error: err
+    })
+  }
 });
 
 module.exports = router;
