49a2d5d651ce3277f7bbcc28bd6ccf0f543ba31d
MKaramen <matis.kara@gmail.com>
2019-11-28 16:28:56 +0100

    order crud done

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,16 +3,29 @@ const router = express.Router();
 const mongoose = require('mongoose')
 
 const Order = require('../models/order')
+const Product = require('../models/product')
 
 router.get("/", async (req, res, next) => {
-  await Order.find()
-  res.status(200).json({
-    message: "Orders were fetched"
-  });
+  try {
+    const all_order = await Order.find().select('_id quantity product')
+    res.status(200).json({
+      message: "Orders were fetched",
+      count: all_order.length,
+      orders: all_order
+    });
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      error: err
+    })
+  }
+
 });
 
 router.post("/", async (req, res, next) => {
   try {
+    await Product.findById(req.body.productId);
+
     const newOrder = new Order({
       _id: new mongoose.Types.ObjectId(),
       quantity: req.body.quantity,
@@ -22,30 +35,70 @@ router.post("/", async (req, res, next) => {
     await newOrder.save();
 
     res.status(201).json({
-      message: "Orders were created",
+      message: "Order was created",
       order: newOrder
     });
 
   } catch (err) {
+    if (err.kind == "ObjectId") {
+      res.status(404).json({
+        message: 'Product ID not found'
+      })
+    } else {
+      console.error(err);
+      res.status(500).json({
+        error: err
+      })
+    }
+  }
+});
+
+router.get("/:orderId", async (req, res, next) => {
+
+  try {
+    const id = req.params.orderId;
+    const orderDetails = await Order.findById(id);
+
+    res.status(200).json({
+      message: "Orders details",
+      orderId: orderDetails.id,
+      relatedProductID: orderDetails.product,
+      quantity: orderDetails.quantity
+    });
+
+  } catch (err) {
+    if (err.kind == "ObjectId") {
+      res.status(404).json({
+        error: "Order ID not found"
+      })
+    }
     console.error(err);
     res.status(500).json({
       error: err
     })
   }
+
 });
 
-router.get("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "Orders details",
-    orderId: req.params.orderId
-  });
-});
+router.delete("/:orderId", async (req, res, next) => {
+  try {
+    const id = req.params.orderId;
+    console.log(id);
 
-router.delete("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "Orders deleted",
-    orderId: req.params.orderId
-  });
+    await Order.deleteOne({
+      _id: id
+    })
+    res.status(200).json({
+      message: "Orders deleted",
+      orderId: req.params.orderId
+    });
+
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({
+      error: err
+    })
+  }
 });
 
 module.exports = router;
